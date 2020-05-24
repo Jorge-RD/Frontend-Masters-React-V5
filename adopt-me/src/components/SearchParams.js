c844aa5a28afff2dd44353fd1a22fbf3fb01fa2a
Jorge-RD <j.rodriguez@intellisys.com.do>
2020-05-24 17:01:24 -0400

    Add SearchParmans and modify scripts

--- adopt-me/src/components/SearchParams.js
+++ adopt-me/src/components/SearchParams.js
@@ -3,10 +3,22 @@ import { ANIMALS } from "@frontendmasters/pet";
 
 export default function SearchParams() {
   const [location, setLocation] = useState("Santo Domingo");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
-  const handleChange = (event) => {
+  const handleChangeLocation = (event) => {
     setLocation(event.target.value);
   };
+
+  const handleChangeAnimal = (event) => {
+    setAnimal(event.target.value);
+  };
+
+  const handleChangeBreed = (event) => {
+    setBreed(event.target.value);
+  };
+
   return (
     <div className="search-params">
       <form>
@@ -15,9 +27,43 @@ export default function SearchParams() {
             id="location"
             value={location}
             placeholder="Location"
-            onChange={handleChange}
+            onChange={handleChangeLocation}
           />
         </label>
+        <label>
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={handleChangeAnimal}
+            onBlur={handleChangeAnimal}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal, key) => (
+              <option value={animal} key={key}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label>
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={handleChangeBreed}
+            onBlur={handleChangeBreed}
+            disabled={!breeds}
+          >
+            <option>All</option>
+            {breeds.map((breed, key) => (
+              <option value={breed} key={key}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
     </div>
