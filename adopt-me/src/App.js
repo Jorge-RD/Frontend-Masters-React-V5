994125fef6982ba07067ac3a3a026fb88365847f
Jorge-RD <j.rodriguez@intellisys.com.do>
2020-05-24 16:00:33 -0400

    Create components App.js and Pet.js

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,1 +1,15 @@
 import React from "react";
+import { render } from "react-dom";
+
+import Pet from "./components/Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me</h1>
+      <Pet name={"Luna"} animal={"Dog"} breed={"Havanese"} />
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
